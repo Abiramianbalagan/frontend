1601a729aaf486f155778bbb4f3d09c51493ee24
Abiramianbalagan <abiramianbalagan79@gmail.com>
2024-03-09 03:46:51 -0800

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://img.lovepik.com/free-png/20211229/lovepik-group-furniture-png-image_401014555_wh300.png" />
+    </div>
+  )
+}
